36ed4b698e05658ee34f29f2665904e97e203ac4
Johanni Brea <jbrea@users.noreply.github.com>
2018-08-22 09:40:52 +0200

    precompile

--- src/ViZDoom.jl
+++ src/ViZDoom.jl
@@ -1,8 +1,11 @@
-__precompile__(false)
 module ViZDoom
 
 using CxxWrap
 @wrapmodule(joinpath(@__DIR__, "..", "deps", "usr", "ViZDoom-1.1.6", "bin", "libvizdoomjl.so"), :ViZDoom)
+
+function __init__()
+    @initcxx
+end
 
 include("util.jl")
 export get_scenario_path, set_game
