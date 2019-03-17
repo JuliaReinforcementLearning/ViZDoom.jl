a2ca6b36754669e9f53409c41887768c69786653
Jun Tian <tianjun.cpp@gmail.com>
2019-03-17 23:07:34 +0800

    update travis, enable test

--- src/ViZDoom.jl
+++ src/ViZDoom.jl
@@ -1,7 +1,7 @@
 module ViZDoom
 
 using CxxWrap
-@wrapmodule(joinpath(@__DIR__, "..", "deps", "usr", "ViZDoom-1.1.6", "bin", "libvizdoomjl.so"), :ViZDoom)
+@wrapmodule(joinpath(@__DIR__, "..", "deps", "usr", "ViZDoom-1.1.6", "bin", "libvizdoomjl.so"))
 
 function __init__()
     @initcxx
